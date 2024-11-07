69ef4faf3083c019ad1732e60ab56f9cd5349a10
beec1000 <beec1000.minecraft@gmail.com>
2024-11-07 11:21:44 +0100

    Add project files.

---
+++ SZGYA13C_Oscar/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SZGYA13C_Oscar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void keresettFilmTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (keresettFilmTB.Text != string.Empty)
+            {
+                placeholder1.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                placeholder1.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ SZGYA13C_Oscar/Oscar.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SZGYA13C_Oscar
+{
+    internal class Oscar
+    {
+        public string Azonosito { get; set; }
+        public string Cim { get; set; }
+        public int Ev { get; set; }
+        public int Dij { get; set; }
+        public int Jelolt { get; set; }
+
+        public Oscar(string azonosito, string cim, int ev, int dij, int jelolt) 
+        { 
+            Azonosito = azonosito;
+            Cim = cim;
+            Ev = ev;
+            Dij = dij;
+            Jelolt = jelolt;
+        }
+
+        public static List<Oscar> FromFile(string path)
+        {
+            List<Oscar> oscar = new List<Oscar>();
+
+            string[] line = File.ReadAllLines(path);
+
+            foreach (var l in line)
+            {
+                string[] s = l.Split("  ");
+
+                string Azonosito = s[0];
+                string Cim = s[1];
+                int Ev = int.Parse(s[2]);
+                int Dij = int.Parse(s[3]);
+                int Jelolt = int.Parse(s[4]);
+
+                Oscar oscars = new(Azonosito, Cim, Ev, Dij, Jelolt);
+
+                oscar.Add(oscars);
+            }
+
+            return oscar;
+        }
+
+        public override string ToString()
+        {
+            return $"{Azonosito}    {Cim}   {Ev}    {Dij}   {Jelolt}";
+        }
+    }
+}
